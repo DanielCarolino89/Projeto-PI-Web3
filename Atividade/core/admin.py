f171ed841d6655e1eaab963ce5aa800c0b281d73
Daniel de Godoy Carolino <99697178+DanielCarolino89@users.noreply.github.com>
2023-06-07 19:06:21 -0300

    Delete Atividade directory

--- Atividade/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import AtividadeModel
-
-class AtividadeModelAdmin(admin.ModelAdmin):
-    list_display = ('titulo','data','local','tarefa')
-    date_hierarchy = 'data'
-    search_fields = ('titulo','data')
-
-admin.site.register(AtividadeModel, AtividadeModelAdmin)
